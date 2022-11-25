4b48b22c24fc494bf1b1244870996b5ec9b3aa67
Robyn <robynmakes@gmail.com>
2022-11-25 13:30:27 -0600

    Started converting scraper.py to class structure

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 import scraper
-import math_utils as maths
-
-# Press the green button in the gutter to run the script.
+import argparse
 if __name__ == '__main__':
-    url = input("URL: ")
+    search = input("Search Term: ")
     amount = int(input("Amount: "))
 
-    scraped_array = scraper.scrape(url, amount)
+    scrape = scraper.Scraper(search, amount)
 
-    for item in scraped_array:
-      print(item)
-
-    print('Average: ' + str(scraper.get_average(prices=scraper.get_prices(scraped_array))))
-    print('Median: ' + str(scraper.get_median(prices=scraper.get_prices(scraped_array))))
-    print('Max: ' + str(scraper.get_max(prices=scraper.get_prices(scraped_array))))
-    print('Min: ' + str(scraper.get_min(prices=scraper.get_prices(scraped_array))))
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- scraper.py
+++ scraper.py
@@ -3,70 +3,64 @@ import math_utils as maths
 from bs4 import BeautifulSoup
 
 
-def scrape(search, amount):
-    url = "http://www.ebay.com/sch/i.html?_from=R40&_nkw=" + search + "&LH_Sold=1"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    items = soup.findAll('div', 's-item__title')
-    prices = soup.findAll('span', 's-item__price')
-
-    titles = []
-    costs = []
-
-    for item in items:
-        titles.append(item.get_text())
-
-    for price in prices:
-        costs.append(price.get_text())
-
-    i = 0
-    final_array = []
-
-    while i < amount:
-        final_array.append([titles[i], costs[i]])
-        i += 1
-
-    final_array.pop(0)
-    return final_array
-
-
-def get_average(prices):
-    array = []
-    for price in prices:
-        x = price.replace('$', '')
-        array.append(float(x))
-    return maths.average(array)
-
-
-def get_median(prices):
-    array = []
-    for price in prices:
-        x = price.replace('$', '')
-        array.append(float(x))
-    return maths.median(array)
-
-
-def get_max(prices):
-    array = []
-    for price in prices:
-        x = price.replace('$', '')
-        array.append(float(x))
-    return max(array)
-
-
-def get_min(prices):
-    array = []
-    for price in prices:
-        x = price.replace('$', '')
-        array.append(x)
-    return min(array)
-
-
-def get_prices(data):
+class Scraper:
+    scraped_array = []
+    url = ""
+    soup = BeautifulSoup()
+    items = []
     prices = []
+    listings = []
 
-    for x in data:
-        prices.append(x[1])
+    def __init__(self, search, amount):
+        search = search.replace(' ', '+')
+        self.url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + search + "&LH_Sold=1"
+        response = requests.get(self.url)                               # obtaining html code from the url
 
-    return prices
+        self.soup = BeautifulSoup(response.text, 'html.parser')
+
+        self.items = self.soup.findAll('div', 's-item__title')          # the items array holds each item's title
+        self.prices = self.soup.findAll('span', 's-item__price')        # the prices array hold each item's price
+
+        i = 0
+        while i < amount:                                               # while loop populates the listing array with listings
+            self.listings.append([self.items[i], self.prices[i]])
+            i += 1
+
+        self.listings.pop(0)                                            # Removing the first item in the array as it isn't actually a listing
+
+    def get_average(self):
+        array = []
+        for price in self.prices:
+            x = price.replace('$', '')
+            array.append(float(x))
+        return maths.average(array)
+
+    def get_median(self):
+        array = []
+        for price in self.prices:
+            x = price.replace('$', '')
+            array.append(float(x))
+        return maths.median(array)
+
+    def get_max(self):
+        array = []
+        for price in self.prices:
+            x = price.replace('$', '')
+            array.append(float(x))
+        return max(array)
+
+    def get_min(self):
+        array = []
+        for price in self.prices:
+            x = price.replace('$', '')
+            array.append(x)
+        return min(array)
+
+    def get_prices(self):
+        return self.prices
+
+    def get_items(self):
+        return self.items
+
+    def get_listings(self):
+        return self.listings
