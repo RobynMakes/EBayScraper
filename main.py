e6e9307c3a35e38c0b395e8ecefd53d49d645ef1
Robyn <robynmakes@gmail.com>
2022-11-30 12:56:35 -0600

    Squashed bugs & optimized code

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import scraper
 import argparse
+
 if __name__ == '__main__':
     search = input("Search Term: ")
     amount = int(input("Amount: "))
 
     scrape = scraper.Scraper(search, amount)
 
-    print(scrape.get_listings())
+    for listing in scrape.get_listings():
+        print(listing)
+
+    print("Average: " + str(scrape.get_average()))
+    print("Median: " + str(scrape.get_median()))
+    print("Max: " + str(scrape.get_max()))
+    print("Min: " + str(scrape.get_min()))
--- scraper.py
+++ scraper.py
@@ -3,6 +3,9 @@ import requests
 import math_utils as maths
 from bs4 import BeautifulSoup
 
+SOLD = 0
+UNSOLD = 1
+
 
 class Scraper:
     scraped_array = []
@@ -24,48 +27,80 @@ class Scraper:
         """
         search = search.replace(' ', '+')
         self.url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + search + "&LH_Sold=1"
-        response = requests.get(self.url)                               # obtaining html code from the url
+        response = requests.get(self.url)  # obtaining html code from the url
 
         self.soup = BeautifulSoup(response.text, 'html.parser')
 
-        self.items = self.soup.findAll('div', 's-item__title')          # the items array holds each item's title
-        self.prices = self.soup.findAll('span', 's-item__price')        # the prices array hold each item's price
+        self.items = self.soup.findAll('div', 's-item__title')  # the items array holds each item's title
+        self.prices = self.soup.findAll('span', 's-item__price')  # the prices array hold each item's price
 
-        i = 0
-        while i < amount:                                               # while loop populates the listing array with listings
+        for i in range(len(self.items)):
+            self.items[i] = self.items[i].text
+
+        for i in range(len(self.prices)):
+            self.prices[i] = self.prices[i].text
+
+        for i in range(amount):  # for loop populates the listing array with listings
+            self.items[i] = self.items[i].replace('<div class="s-item__title"><span aria-level="3" role="heading">', '')
+            self.items[i] = self.items[i].replace('</span></div>', '')
+            self.prices[i] = self.prices[i].replace('<span class="s-item__price"><span class="POSITIVE ITALIC">', '')
+            self.prices[i] = self.prices[i].replace('</span></span>', '')
             self.listings.append([self.items[i], self.prices[i]])
             i += 1
 
-        self.listings.pop(0)                                            # Removing the first item in the array as it isn't actually a listing
+        self.listings.pop(0)  # Removing the first item in the array as it isn't actually a listing
 
     def get_average(self):
         """Returns the average of the prices array"""
         array = []
-        for price in self.prices:
-            x = price.replace('$', '')
-            array.append(float(x))
+        for price in self.listings:
+            x = price[1].replace('$', '')
+            try:
+                array.append(float(x))
+            except ValueError:
+                print("Value Error: could not convert string to float: '" + str(price) + "'")
+                continue
+            else:
+                array.append(float(x))
         return maths.average(array)
 
     def get_median(self):
         """Returns the median of the prices array"""
         array = []
-        for price in self.prices:
-            x = price.replace('$', '')
-            array.append(float(x))
+        for price in self.listings:
+            try:
+                x = price[1].replace('$', '')
+            except ValueError:
+                print("Value Error: could not convert string to float: '" + str(price) + "'")
+                continue
+            else:
+                array.append(float(x))
         return maths.median(array)
 
     def get_max(self):
         """Returns the maximum of the prices array"""
         array = []
-        for price in self.prices:
-            x = price.replace('$', '')
-            array.append(float(x))
+        for price in self.listings:
+            try:
+                x = price[1].replace('$', '')
+            except ValueError:
+                print("Value Error: could not convert string to float: '" + str(price) + "'")
+            else:
+                array.append(float(x))
         return max(array)
 
     def get_min(self):
         """Returns the minimum of the prices array"""
         array = []
-        for price in self.prices:
-            x = price.replace('$', '')
-            array.append(x)
+        for price in self.listings:
+            try:
+                x = price[1].replace('$', '')
+            except ValueError:
+                print("Value Error: could not convert string to float: '" + str(price) + "'")
+            else:
+                array.append(float(x))
         return min(array)
+
+    def get_listings(self):
+        """Returns the listing array"""
+        return self.listings
